20147d9bcb49afe26784ea894a20debafcbba2b3
Sinyii <shuan123@asu.edu>
2019-04-20 00:01:07 -0400

    reupdate

---
+++ weatherAppTab/app/src/main/java/com/sinyi/weatherapptab/CurrentTemp.java
@@ -0,0 +1,208 @@
+package com.sinyi.weatherapptab;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link CurrentTemp.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link CurrentTemp#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+
+public class CurrentTemp extends Fragment {
+    String city = MainActivity.CITY;
+    String WEATHERMAP_API_KEY = MainActivity.WEATHERMAP_API_KEY;
+    TextView cityNameField, detailField, currentTemperatureField,
+            humidityField, pressureField, weatherIcon, updatedField;
+    ProgressBar progressBar;
+    Typeface weatherFont;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public CurrentTemp() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @return A new instance of fragment CurrentTemp.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CurrentTemp newInstance(String param1) {
+        CurrentTemp fragment = new CurrentTemp();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            city = getArguments().getString(ARG_PARAM1);
+        }
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_current_temp, container, false);
+        progressBar = (ProgressBar) v.findViewById(R.id.progressBar);
+        cityNameField = (TextView) v.findViewById(R.id.cityNameField);
+        updatedField = (TextView) v.findViewById(R.id.updatedField);
+        detailField = (TextView) v.findViewById(R.id.detailField);
+        currentTemperatureField = (TextView) v.findViewById(R.id.currentTemperatureField);
+        humidityField = (TextView) v.findViewById(R.id.humidityField);
+        pressureField = (TextView) v.findViewById(R.id.pressureField);
+        weatherIcon = (TextView) v.findViewById(R.id.weatherIcon);
+        weatherFont = Typeface.createFromAsset(getActivity().getAssets(), "fonts/weathericons-regular-webfont.ttf");
+        weatherIcon.setTypeface(weatherFont);
+        taskLoadUp(city);
+
+        // Inflate the layout for this fragment
+        return v;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+
+
+
+    public void taskLoadUp(String query) {
+        if (Function.isNetworkAvailable(getActivity().getApplicationContext())) {
+            DownloadWeather task = new DownloadWeather();
+            task.execute(query);
+        } else {
+            Toast.makeText(getActivity().getApplicationContext(), "No Internet Connection", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    class DownloadWeather extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+
+        }
+
+        protected String doInBackground(String... args) {
+            String xml = Function.excuteGet("https://api.openweathermap.org/data/2.5/weather?q=" + args[0] +
+                    "&units=imperial&appid=" + WEATHERMAP_API_KEY);
+            if (xml == null) {
+                return "";
+            }
+            return xml;
+        }
+
+        @Override
+        protected void onPostExecute(String xml) {
+
+            try {
+                JSONObject json = new JSONObject(xml);
+                if (json != null) {
+                    JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+                    JSONObject main = json.getJSONObject("main");
+                    DateFormat df = DateFormat.getDateTimeInstance();
+
+                    cityNameField.setText(json.getString("name").toUpperCase(Locale.US) + ", " + json.getJSONObject("sys").getString("country"));
+                    detailField.setText(details.getString("description").toUpperCase(Locale.US));
+                    currentTemperatureField.setText(String.format("%.2f", main.getDouble("temp")) + "°");
+                    humidityField.setText("Humidity: " + main.getString("humidity") + "%");
+                    pressureField.setText("Pressure: " + main.getString("pressure") + " hPa");
+                    updatedField.setText(df.format(new Date(json.getLong("dt") * 1000)));
+                    weatherIcon.setText(Html.fromHtml(Function.setWeatherIcon(details.getInt("id"),
+                            json.getJSONObject("sys").getLong("sunrise") * 1000,
+                            json.getJSONObject("sys").getLong("sunset") * 1000)));
+
+                    progressBar.setVisibility(View.GONE);
+
+                }
+            } catch (JSONException e) {
+                Toast.makeText(getActivity().getApplicationContext(), "Error, Check City: " + city, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
